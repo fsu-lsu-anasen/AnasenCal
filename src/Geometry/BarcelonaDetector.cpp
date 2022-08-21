84188aac26977f75922d4add9abcdaa676128f66
anasendaq <anasendaq@gmail.com>
2022-08-21 16:59:28 -0700

    Starting geometry implementation

---
+++ src/Geometry/BarcelonaDetector.cpp
@@ -0,0 +1,1 @@
+#include "BarcelonaDetector.h"
---
+++ src/Geometry/BarcelonaDetector.h
@@ -0,0 +1,22 @@
+#ifndef BARCELONA_DETECTOR_H
+#define BARCELONA_DETECTOR_H
+
+#include <cmath>
+#include <vector>
+
+#include "Math/Point3D.h"
+#include "Math/Vector3D.h"
+#include "Math/RotationZ.h"
+
+class BarcelonaDetector
+{
+public:
+    BarcelonaDetector(double length, double width, double centerPhi, double centerZ, double centerRho);
+    ~BarcelonaDetector();
+
+    
+
+private:
+};
+
+#endif
